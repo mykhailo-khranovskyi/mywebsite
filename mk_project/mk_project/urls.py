acae2447205c391363497bcc3c256a541d52e41f
Mykhailo Khranovskyi <mykhailo.khranovskyi@djangostars.com>
2023-03-24 23:43:44 +0200

    add home

--- mk_project/mk_project/urls.py
+++ mk_project/mk_project/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('myapp/', include('myapp.urls')),
+    path('index/', include('myapp.urls')),
+    path('home/', include('myapp.urls')),
     path('admin/', admin.site.urls),
 ]
--- mk_project/myapp/views.py
+++ mk_project/myapp/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the myapp index.")
+    return HttpResponse("Hello, world. You're at the index page.")
+
+
+def home(request):
+    return render(request, 'home.html')
